f497eede127934a07ed869c4cf439d82e2398bb7
nayanpai <nayanspai@gmail.com>
2024-08-13 23:03:18 -0700

    Problem 724

---
+++ 724 - Find Pivot Index/PivotIndex.py
@@ -0,0 +1,15 @@
+class Solution:
+    def pivotIndex(self, nums: [int]) -> int:
+        if len(nums) == 1:
+            return 0
+
+        if len(nums) == 2:
+            return index(0)
+
+        for i in range(len(nums)):
+            if i == 0 and sum(nums[1:]) == 0:
+                return 0
+            if sum(nums[i+1:]) == sum(nums[:i+1]):
+                return 1
+
+        return -1
